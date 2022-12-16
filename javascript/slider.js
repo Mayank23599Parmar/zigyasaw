709b4891f4ff471e0e6df537f02166921291e373
Harshad Lucent <harshad@lucentinnovation.com>
2022-12-17 01:31:02 +0530

    Home Slider

--- javascript/slider.js
+++ javascript/slider.js
@@ -1,8 +1,28 @@
-import Swiper from './vendor/swiper';
-var swiper = new Swiper(".collection-slider-wrapper", {
-    slidesPerView: 3,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+import Swiper from "./vendor/swiper";
+var swiper = new Swiper(".collection-feature-slider-wrapper", {
+  slidesPerView: 1,
+  height: "auto",
+  // effect: "fade",
+  spaceBetween: 60,
+  centeredSlides: true,
+  initialSlide: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-  });
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+      initialSlide: 3,
+    },
+  },
+});
